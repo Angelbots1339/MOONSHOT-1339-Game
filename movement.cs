794318a3685c4a2f5c0d20b6e1d337567515e082
Devon <devonrutledge03@gmail.com>
2020-11-17 20:02:30 -0700

    shh

--- movement.cs
+++ movement.cs
@@ -3,35 +3,35 @@ using System;
 
 public class movement : RigidBody2D
 {
-    [Export] public int speed = 50;
-    [Export] public float deceleration = 0.3f;
+	[Export] public int speed = 50;
+	[Export] public float deceleration = 0.3f;
 
-    public Vector2 force = new Vector2();
+	public Vector2 force = new Vector2();
 
-    public void GetInput()
-    {
-        if (Input.IsActionPressed("left"))
-            force += new Vector2(-speed, 0).Rotated(Rotation);
+	public void GetInput()
+	{
+		if (Input.IsActionPressed("left"))
+			force += new Vector2(-speed, 0).Rotated(Rotation);
 
-        if (Input.IsActionPressed("right"))
-            force += new Vector2(speed, 0).Rotated(Rotation);
+		if (Input.IsActionPressed("right"))
+			force += new Vector2(speed, 0).Rotated(Rotation);
 
-        force -= LinearVelocity * deceleration;
-    }
+		force -= LinearVelocity * deceleration;
+	}
 
-    public void MoveToGrav()
-    {
-        Node planets = GetParent().GetNode("planets");
-        foreach(Node planet in planets.GetChildren()){
-            force += planet;
-        }
-    }
+	public void MoveToGrav()
+	{
+		Node planets = GetParent().GetNode("planets");
+		foreach(Node planet in planets.GetChildren()){
+			force += planet;
+		}
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-        force = new Vector2();
-        GetInput();
-        MoveToGrav();
-        ApplyCentralImpulse(force);
-    }
+	public override void _PhysicsProcess(float delta)
+	{
+		force = new Vector2();
+		GetInput();
+		MoveToGrav();
+		ApplyCentralImpulse(force);
+	}
 }
