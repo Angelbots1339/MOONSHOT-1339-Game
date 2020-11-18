fd9f0e89458b90662cc83ebe183b318775eeb09f
Devon <devonrutledge03@gmail.com>
2020-11-17 23:02:38 -0700

    physics

--- Gravity.cs
+++ Gravity.cs
@@ -7,7 +7,14 @@ public class Gravity : RigidBody2D
 
     // Called when the node enters the scene tree for the first time.
 
-    public float GetGravity() {
+    public float GetGravity()
+    {
         return gravityForce;
+    }
+
+    public override void _Draw()
+    {
+        var color = new Color(0.5f,0.5f,0.5f,0.5f);
+        DrawCircle(new Vector2(), gravityForce, color);
     }
 }
