d1e9fed3354aed7ff78009b7adeb41426618ac3a
JDykstra13 <38771448+JDykstra13@users.noreply.github.com>
2020-11-24 22:12:37 -0700

    added comments+mouse+planet image

--- Gravity.cs
+++ Gravity.cs
@@ -3,18 +3,18 @@ using System;
 
 public class Gravity : RigidBody2D
 {
-    [Export] public float gravityForce = 40;
+	[Export] public float gravityForce = 40;
 
-    // Called when the node enters the scene tree for the first time.
+	// Called when the node enters the scene tree for the first time.
 
-    public float GetGravity()
-    {
-        return gravityForce;
-    }
+	public float GetGravity()
+	{
+		return gravityForce;
+	}
 
-    public override void _Draw()
-    {
-        var color = new Color(0.5f,0.5f,0.5f,0.5f);
-        DrawCircle(new Vector2(), gravityForce, color);
-    }
+	public override void _Draw()
+	{
+		var color = new Color(0.5f,0.5f,0.5f,0.5f);
+		DrawCircle(new Vector2(), gravityForce, color);
+	}
 }
--- movement.cs
+++ movement.cs
@@ -1,16 +1,17 @@
 using Godot;
 using System;
 
-public class movement : RigidBody2D
+public class movement : RigidBody2D //extends rigidbody2D (instead of extends it's :)
 {
-    [Export] public int speed = 50;
+    [Export] public int speed = 50; //export means it is seen in the godot editor
+    //you can edit these variables in the godot editor
     [Export] public float deceleration = 0.01f;
 
-    public Vector2 force = new Vector2();
+    public Vector2 force = new Vector2(); //this variable can only be accessed in VS
 
     public void GetInput()
     {
-        if (Input.IsActionPressed("left"))
+        if (Input.IsActionPressed("left")) //setting up the input
             force += new Vector2(-speed, 0).Rotated(Rotation);
 
         if (Input.IsActionPressed("right"))
@@ -25,8 +26,8 @@ public class movement : RigidBody2D
         Node planets = GetParent().GetNode("planets");
         foreach (Gravity planet in planets.GetChildren())
         {
-			var outsideness = (planet.GlobalPosition - this.GlobalPosition).Length()/planet.gravityForce;
-            var distanceVector = planet.GlobalPosition - this.GlobalPosition;
+            var distanceVector = planet.GlobalPosition - this.GlobalPosition; 
+			var outsideness = (distanceVector).Length()/planet.gravityForce; //length of how far player/movementscript is from planet
             if(distanceVector.Length() <= planet.gravityForce) force += distanceVector.Normalized() * (10+10/outsideness);
         }
     }
@@ -36,7 +37,7 @@ public class movement : RigidBody2D
         force = new Vector2();
         MoveToGrav();
         if(force.Length() > 0.01){
-            var error = force.Angle() - (float)Math.PI / 2 - Rotation;
+            var error = force.Angle() - (float)Math.PI / 2 - Rotation; //makes it so the player always has it's bottom facing the planet
             if(error > Math.PI) error = 2 * (float) Math.PI - error;
             if(error < -Math.PI) error = 2 * (float) Math.PI + error;
             Rotation += 0.25f * error;
