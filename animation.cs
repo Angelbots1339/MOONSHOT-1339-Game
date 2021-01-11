523de3d0e32cf3a882565425303792c1af667aa8
DPS100 <0dps100@gmail.com>
2021-01-10 20:16:35 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/Angelbots1339/MOONSHOT-1339-Game into main""
    This reverts commit aabdba37f60d0303cb7554a96428236f89ef1b92.

---
+++ animation.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public class animation : KinematicBody2D {
+    public Vector2 player = new Vector2();
+/*
+public void animate(){
+    if (Input.IsActionPressed("left") || Input.IsActionPressed("right")){
+        player.play("walking");
+    }
+       
+    
+}
+   
+
+public void flipPlayer(){
+    animate();
+     if (Input.IsActionPressed("left")){
+         
+     }
+}
+extends KinematicBody2D
+
+func _process(delta):
+    if Input.is_action_pressed("ui_right"):
+        $AnimationPlayer.play("walk")
+    else:
+        $AnimationPlayer.stop()
+        */
+}
