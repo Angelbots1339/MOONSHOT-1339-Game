c9a3521305f8fab14cd304d4b50a4b34a8b88da5
DPS100 <0dps100@gmail.com>
2021-01-11 21:55:38 -0700

    Added exit button and help menu. Closes #15

--- MainMenu.cs
+++ MainMenu.cs
@@ -8,9 +8,25 @@ public class MainMenu : Control
 	// private string b = "text";
 
 	// Called when the node enters the scene tree for the first time.
+	public PopupPanel helpPanel;
 	public override void _Ready()
 	{
-		
+		helpPanel = GetNode<PopupPanel>("Panel/Help/HelpPanel");
+	}
+
+	public void _on_Quit_pressed()
+	{
+		GetTree().Quit();
+	}
+
+	public void _on_Help_pressed()
+	{
+		helpPanel.Visible = true;
+	}
+
+	public void _on_Exit_pressed()
+	{
+		helpPanel.Visible = false;
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
