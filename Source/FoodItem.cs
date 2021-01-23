c00d58a0177488883d7da5c6435184ea5edbfb02
DPS100 <0dps100@gmail.com>
2021-01-22 17:37:40 -0700

    Restore FoodItem.cs

---
+++ Source/FoodItem.cs
@@ -0,0 +1,32 @@
+using System;	
+using System.Collections.Generic;	
+public class FoodItem	
+{	
+    public static readonly FoodItem NONE = new FoodItem("None", "");	
+    public static readonly FoodItem BUN   = new FoodItem("Bun", "assets/Food/bun2.png");	
+    public static readonly FoodItem CHEESE   = new FoodItem("Cheese", "assets/Food/cheese2.png");	
+    public static readonly FoodItem HAMBURGER    = new FoodItem("Hamburger", "assets/Food/hamburger2 copy.png");	
+    public static readonly FoodItem LETTUCE = new FoodItem("Lettuce", "assets/Food/lettuce2.png");	
+    public static readonly FoodItem PATTY  = new FoodItem("Patty", "assets/Food/patty2.png");	
+    public static readonly FoodItem TOMATO  = new FoodItem("Tomato", "assets/Food/tomato2.png");	
+
+    public static IEnumerable<FoodItem> Values	
+    {	
+        get	
+        {	
+            yield return NONE;	
+            yield return BUN;	
+            yield return CHEESE;	
+            yield return HAMBURGER;	
+            yield return LETTUCE;	
+            yield return PATTY;	
+            yield return TOMATO;	
+        }	
+    }	
+
+    public string Name   { get; private set; }	
+    public string ImagePath   { get; private set; }	
+
+    FoodItem(string name, string imagePath) => 	
+        (Name, ImagePath) = (name, imagePath);	
+} 	
