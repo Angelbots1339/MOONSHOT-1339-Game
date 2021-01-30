7c94faaf0cb7475a54477884e1e7e24720889938
DPS100 <0dps100@gmail.com>
2021-01-29 20:51:49 -0700

    Fix file paths

--- Source/FoodItem.cs
+++ Source/FoodItem.cs
@@ -10,12 +10,12 @@ using System.Collections.Generic;
 public class FoodItem	
 {	
     public static readonly FoodItem NONE = new FoodItem("None", "");	
-    public static readonly FoodItem BUN   = new FoodItem("Bun", "assets/Food/bun2.png");	
-    public static readonly FoodItem CHEESE   = new FoodItem("Cheese", "assets/Food/cheese2.png");	
-    public static readonly FoodItem HAMBURGER    = new FoodItem("Hamburger", "assets/Food/hamburger2 copy.png");	
-    public static readonly FoodItem LETTUCE = new FoodItem("Lettuce", "assets/Food/lettuce2.png");	
-    public static readonly FoodItem PATTY  = new FoodItem("Patty", "assets/Food/patty2.png");	
-    public static readonly FoodItem TOMATO  = new FoodItem("Tomato", "assets/Food/tomato2.png");	
+    public static readonly FoodItem BUN   = new FoodItem("Bun", "res://assets/Hamburger/bun2.png");	
+    public static readonly FoodItem CHEESE   = new FoodItem("Cheese", "res://assets/Hamburger/cheese2.png");	
+    public static readonly FoodItem HAMBURGER    = new FoodItem("Hamburger", "res://assets/Hamburger/hamburger2 copy.png");	
+    public static readonly FoodItem LETTUCE = new FoodItem("Lettuce", "res://assets/Hamburger/lettuce2.png");	
+    public static readonly FoodItem PATTY  = new FoodItem("Patty", "res://assets/Hamburger/patty2.png");	
+    public static readonly FoodItem TOMATO  = new FoodItem("Tomato", "res://assets/Hamburger/tomato2.png");	
 
     public static IEnumerable<FoodItem> Values	
     {	
@@ -39,7 +39,7 @@ public class FoodItem
     // Get the texture for the food item
     public Texture GetTexture()
     {
-        return (Texture) GD.Load("res://" + ImagePath);
+        return (Texture) GD.Load(ImagePath);
     }
 
     FoodItem(string name, string imagePath) => 	
