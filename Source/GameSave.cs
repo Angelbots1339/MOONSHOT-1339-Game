5d7e18f5ce18530e90ee6eed5f48792f67459fd0
Owen Harrison <owenbharrison@gmail.com>
2021-02-05 17:25:08 -0700

    added GameSave.load() FOR WINDOWS ONLY

--- Source/GameSave.cs
+++ Source/GameSave.cs
@@ -1,11 +1,21 @@
 using Godot;
 using System;
 class GameSave : Node{
-  public void saveGame(){
+  public void saveGame_WINDOWS(){
     Player p = GetTree().Root.GetNode("Node2D").GetNode<Player>("Player");
-    string posx = "PosX: "+p.Position.x;
-    string posy = "PosY: "+p.Position.y;
-    string rot = "Rot: "+p.Rotation;
+    string posx = ""+p.Position.x;
+    string posy = ""+p.Position.y;
+    string rot = ""+p.Rotation;
     System.IO.File.WriteAllText(@"C:\Users\Public\OvercookedSaveData\save_test.txt", posx+"\n"+posy+"\n"+rot);
+  }
+
+  public void loadGame_WINDOWS(){
+    Player p = GetTree().Root.GetNode("Node2D").GetNode<Player>("Player");
+    String loaded = System.IO.File.ReadAllText(@"C:\Users\Public\OvercookedSaveData\save_test.txt");
+    String[] args = loaded.Split("\n");
+    float x = float.Parse(args[0]);
+    float y = float.Parse(args[1]);
+    p.Position = new Godot.Vector2(x, y);
+    p.Rotation = float.Parse(args[2]);
   }
 }
