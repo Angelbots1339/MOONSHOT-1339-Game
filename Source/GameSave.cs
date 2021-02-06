a0d59e05c3583576d63d4069b4234eac1c370708
Owen Harrison <owenbharrison@gmail.com>
2021-02-05 17:43:41 -0700

    refactored saving to woork with all os

--- Source/GameSave.cs
+++ Source/GameSave.cs
@@ -12,12 +12,12 @@ class GameSave : Node{
     string posx = ""+p.Position.x;
     string posy = ""+p.Position.y;
     string rot = ""+p.Rotation;
-    System.IO.File.WriteAllText(@"C:\Users\Public\OvercookedSaveData\save_test.txt", posx+"\n"+posy+"\n"+rot);
+    System.IO.File.WriteAllText("user://OvercookedSaveData.txt", posx+"\n"+posy+"\n"+rot);
   }
 
   public void loadGame_WINDOWS(){
     Player p = GetTree().Root.GetNode("Node2D").GetNode<Player>("Player");
-    String loaded = System.IO.File.ReadAllText(@"C:\Users\Public\OvercookedSaveData\save_test.txt");
+    String loaded = System.IO.File.ReadAllText("user://OvercookedSaveData.txt");
     String[] args = loaded.Split("\n");
     float x = float.Parse(args[0]);
     float y = float.Parse(args[1]);
