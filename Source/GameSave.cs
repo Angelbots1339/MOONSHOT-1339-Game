a57be609abd17110d5e0ac7f4f23acdf08114908
Owen Harrison <owenbharrison@gmail.com>
2021-02-09 17:46:18 -0700

    refactored saving code and added some coments

--- Source/GameSave.cs
+++ Source/GameSave.cs
@@ -11,19 +11,20 @@ only works for position and rotation right now
 class GameSave : Node{
   public void saveGame(){
     Player p = GetTree().Root.GetNode("Node2D").GetNode<Player>("Player");
-    string posx = ""+p.Position.x;
-    string posy = ""+p.Position.y;
-    string rot = ""+p.Rotation;
-    System.IO.File.WriteAllText("user://OvercookedSaveData.txt", posx+"\n"+posy+"\n"+rot);
+    float[] args = {p.Position.x, p.Position.y, p.Rotation};//add to that list: Godot.currentLevel() like function to keep track of progress
+    //string currentLvl = Godot.getCurrentLevel();
+    System.IO.File.WriteAllText("user://OvercookedSaveData.txt", String.Join("\n", args));//+"\n"+currentLvl
   }
 
   public void loadGame(){
     Player p = GetTree().Root.GetNode("Node2D").GetNode<Player>("Player");
     String loaded = System.IO.File.ReadAllText("user://OvercookedSaveData.txt");
-    String[] args = loaded.Split("\n");
+    string[] args = loaded.Split("\n");
+
     float x = float.Parse(args[0]);
     float y = float.Parse(args[1]);
     p.Position = new Godot.Vector2(x, y);
     p.Rotation = float.Parse(args[2]);
+    //Godot.setCurrentLevel(int.Parse(args[3]));
   }
 }
